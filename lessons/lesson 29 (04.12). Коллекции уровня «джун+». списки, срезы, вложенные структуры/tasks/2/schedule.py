ae61197b4a48a08b942163e34f0e58232bbc2f4a
SaifutdinovMars <marssaifutdinov@gmail.com>
2025-12-04 21:37:30 +0500

    Create schedule.py

---
+++ lessons/lesson 29 (04.12). Коллекции уровня «джун+». списки, срезы, вложенные структуры/tasks/2/schedule.py
@@ -0,0 +1,5 @@
+schedule = ["07:05", "07:23", "07:41", "07:59", "08:17", "08:35", "08:53", "09:11", "09:29"]
+slice_of_schedule = schedules[:2]
+print(slice_of_schedule[:3])
+print(schedule[5:len(schedule)-1])
+print(schedule[2:len(schedule)6])
